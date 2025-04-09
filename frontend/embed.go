d7018cb03393b765772f26472282aefda00ca109
Bryan Rose <bryan@bryanrose.net>
2025-04-09 08:25:17 -0600

    build static files for SPA

--- frontend/embed.go
+++ frontend/embed.go
@@ -6,8 +6,8 @@ import (
 	"io/fs"
 )
 
-//go:embed src/*
+//go:embed build/*
 var distDir embed.FS
 
 // DistDirFS contains the embedded dist directory files (without the "dist" prefix)
-var DistDirFS, _ = fs.Sub(distDir, "dist")
+var DistDirFS, _ = fs.Sub(distDir, "build")
